249d24abfb78c033b0d267af628012bf12e5c267
Chigozie Oduah <oduahchigozie46@gmail.com>
2025-07-12 19:19:11 +0100

    added while loop, and exit handler

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h> 
 #include <netdb.h> 
 #include <netinet/in.h> 
@@ -6,44 +7,63 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #include <unistd.h> // read(), write(), close()
-#define MAX 80 
+#define MAX 1024 
 #define PORT 8080 
 #define SA struct sockaddr 
   
 // Function designed for chat between client and server. 
 void func(int connfd) 
 { 
-    char buff[MAX]; 
-    int n; 
-    // infinite loop for chat 
-    for (;;) { 
-        bzero(buff, MAX); 
-  
-        // read the message from client and copy it in buffer 
-        read(connfd, buff, sizeof(buff)); 
-        // print buffer which contains the client contents 
-        printf("From client: %s\t To client : ", buff); 
-        bzero(buff, MAX); 
-        n = 0; 
-        // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-  
-        // and send that buffer to client 
-        write(connfd, buff, sizeof(buff)); 
-  
-        // if msg contains "Exit" then server exit and chat ended. 
-        if (strncmp("exit", buff, 4) == 0) { 
-            printf("Server Exit...\n"); 
-            break; 
-        } 
-    } 
+    char in_buff[MAX]; 
+    bzero(in_buff, MAX); 
+
+    char out_buff[MAX] = "HTTP/1.1 200 OK\n"
+        "Date: Mon, 27 Jul 2009 12:28:53 GMT\n"
+        "Server: Apache\n"
+        "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n"
+        "ETag: \"34aa387-d-1568eb00\"\n"
+        "Accept-Ranges: bytes\n"
+        "Content-Length: 51\n"
+        "Vary: Accept-Encoding\n"
+        "Content-Type: text/plain\n"
+        "\n"
+        "Hello World! My content includes a trailing CRLF.\n";
+
+
+    int n = 0; 
+
+    // read the message from client and copy it in buffer 
+    read(connfd, in_buff, sizeof(in_buff)); 
+
+    // print buffer which contains the client contents 
+    printf("From client: %s\n", in_buff); 
+
+    // copy server message in the buffer 
+    // while ((buff[n++] = getchar()) != '\n') 
+    //     ; 
+
+    // and send that buffer to client 
+    write(connfd, out_buff, sizeof(out_buff)); 
+
+    // if msg contains "Exit" then server exit and chat ended. 
+    // if (strncmp("exit", buff, 4) == 0) { 
+    //     printf("Server Exit...\n"); 
+    //     break; 
+    // } 
 } 
+
+int sockfd;
+
+void handle_exit(int code) {
+    printf("Gracefully shutting down...\n");
+    close(sockfd); 
+    exit(0);
+}
   
 // Driver function 
 int main() 
 { 
-    int sockfd, connfd, len; 
+    int connfd, len; 
     struct sockaddr_in servaddr, cli; 
   
     // socket create and verification 
@@ -56,6 +76,8 @@ int main()
         printf("Socket successfully created..\n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
+    signal(SIGINT, handle_exit);
+
     // assign IP, PORT 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
@@ -78,18 +100,18 @@ int main()
         printf("Server listening..\n"); 
     len = sizeof(cli); 
   
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, (unsigned int*) &len); 
-    if (connfd < 0) { 
-        printf("server accept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server accept the client...\n"); 
-  
-    // Function for chatting between client and server 
-    func(connfd); 
-  
-    // After chatting close the socket 
-    close(sockfd); 
+    while(1) {
+        // Accept the data packet from client and verification 
+        connfd = accept(sockfd, (SA*)&cli, (unsigned int*) &len); 
+        if (connfd < 0) { 
+            printf("server accept failed...\n"); 
+            continue;
+        } 
+        else printf("server accept the client...\n"); 
+      
+        // Function for chatting between client and server 
+        func(connfd); 
+      
+        // After chatting close the socket 
+    }
 }
